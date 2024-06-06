237ba77c194b415feb757c8255426cbee2039666
shweta2003 <shwetakamal4714@gmail.com>
2024-06-06 13:30:39 +0530

    first commit

---
+++ src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id:1,
+        text: " first comments",
+    },
+    {
+        id:2,
+        text:"second comments",
+    },
+    {
+        id:3,
+        text: "third comments",
+    },
+]
